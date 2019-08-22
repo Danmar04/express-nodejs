cbae23482238a8d64008c2e1b57624522bc82d5d
daniel.martin <daniel.martin@babel.es>
2019-08-22 09:42:19 +0200

    First version

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+//MIDDLEWARES
 //Parser bodies
 app.use(bodyParser.urlencoded({ extended: false }));
+//Sirve las carpetas estaticas
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', (req, res, next) => {
-    console.log('Im the first middleware');
-    next();
-});
+app.use('/admin', adminRoutes);
 
-app.use('/users', (req, res, next) => {
-    res.send('<h1>This is the second  middleware</h1>');
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
