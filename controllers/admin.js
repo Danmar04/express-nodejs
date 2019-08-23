491e9f4660e8135efb6c68c2deed76d0662672d0
daniel.martin <daniel.martin@babel.es>
2019-08-23 10:27:20 +0200

    MVC Shop Structure

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,8 @@
-
 const Product = require('../models/product');
 
+//Redireccion a introducir nuevos productos
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product',
+    res.render('admin/add-product',
         {
             pageTitle: 'Add Product',
             path: '/admin/add-product',
@@ -12,8 +12,13 @@ exports.getAddProduct = (req, res, next) => {
         });
 }
 
+//Añadir nuevo producto
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 }
@@ -21,11 +26,11 @@ exports.postAddProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
         res.render(
-            'shop',
+            'admin/products',
             {
                 prods: products,
-                pageTitle: 'Shop',
-                path: '/',
+                pageTitle: 'Admin Products',
+                path: '/admin/products',
                 hasProducts: products.length > 0,
                 activeShop: true,
                 productCSS: true
@@ -33,5 +38,4 @@ exports.getProducts = (req, res, next) => {
             }
         );
     });
-
 };
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,20 @@ const path = require('path');
 
 const express = require('express'); 7
 
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
 
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+// TODO: Implementar
+router.get('/products', shopController.getProducts);
+router.get('/cart', shopController.getCart);
+router.get('/checkout', shopController.getCheckout);
+router.get('/orders', shopController.getOrders);
+
+// router.get('/product-detail', METODO_DETALLE_PRODUCTO)
 
 module.exports = router;
